5f2aab1c8fcdf7527ea1818955b7d459d37565e2
Ayush6818 <ayush.wwekumar2633@gmail.com>
2026-02-17 11:23:39 +0530

    UC2 update

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,13 @@
-/**
- * OOPSBannerApp
- * UC1: Print OOPS to Console
- * This program prints "OOPS" to the console.
- *
- * @author Ayush
- * @version 1.0
- */
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  *****    *****    ******    *****  ");
+        System.out.println(" **   **  **   **  **    **  **   ** ");
+        System.out.println("**     ****     ****    **  **       ");
+        System.out.println("**     ****     ******   *****   ");
+        System.out.println("**     ****     **            ** ");
+        System.out.println(" **   **  **   **  **    **  **   ** ");
+        System.out.println("  *****    *****    **    **   *****  ");
     }
 }
+// UC2 Banner Done
