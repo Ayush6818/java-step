4c2afd419bda8a0e3c20dca29b06d127059ef0cb
Ayush6818 <ayush.wwekumar2633@gmail.com>
2026-03-02 23:54:08 +0530

    UC4 completed

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,49 +1,25 @@
 public class OOPSBannerApp {
 
-    static String getLine(String pattern, int index) {
-        String[] lines = pattern.split(",");
-        return lines[index];
-    }
-
     public static void main(String[] args) {
 
-        String o = String.join(",",
-                " ***** ",
-                " *   * ",
-                " *   * ",
-                " *   * ",
-                " *   * ",
-                " *   * ",
-                " ***** "
-        );
+        String[] banner = {
+            String.join("   ",
+                " ***** ", " ***** ", " ***** ", " ***** "),
+            String.join("   ",
+                " *   * ", " *   * ", " *   * ", " *     "),
+            String.join("   ",
+                " *   * ", " *   * ", " *   * ", " *     "),
+            String.join("   ",
+                " *   * ", " *   * ", " ***** ", " ***** "),
+            String.join("   ",
+                " *   * ", " *   * ", " *     ", "     * "),
+            String.join("   ",
+                " *   * ", " *   * ", " *     ", "     * "),
+            String.join("   ",
+                " ***** ", " ***** ", " *     ", " ***** ")
+        };
 
-        String p = String.join(",",
-                " ***** ",
-                " *   * ",
-                " *   * ",
-                " ***** ",
-                " *     ",
-                " *     ",
-                " *     "
-        );
-
-        String s = String.join(",",
-                " ***** ",
-                " *     ",
-                " *     ",
-                " ***** ",
-                "     * ",
-                "     * ",
-                " ***** "
-        );
-
-        for (int i = 0; i < 7; i++) {
-            String line = String.join("   ",
-                    getLine(o, i),
-                    getLine(o, i),
-                    getLine(p, i),
-                    getLine(s, i)
-            );
+        for(String line : banner){
             System.out.println(line);
         }
     }
