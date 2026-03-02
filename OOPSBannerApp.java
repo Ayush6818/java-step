4602fad9072bc3f732b03ec2edb1e1cb63a41d3a
Ayush6818 <ayush.wwekumar2633@gmail.com>
2026-03-02 22:53:18 +0530

    Updated OOPS Banner App

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,36 @@
-
-/**
- * OOPSBannerApp
- * UC1: Print OOPS to Console
- * @author Ayush Kumar
- * @version 1.0
- */
-
-public class OOPSBannerApp {
+public class OOPSBannerUC3 {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        String[] o1 = {
+                String.join(" ", "*****", "*   *", "*   *", "*   *", "*   *", "*   *", "*****"),
+        };
+
+        String[] o2 = {
+                String.join(" ", "*****", "*   *", "*   *", "*   *", "*   *", "*   *", "*****"),
+        };
+
+        String[] p = {
+                String.join(" ", "*****", "*   *", "*   *", "*****", "*", "*", "*"),
+        };
+
+        String[] s = {
+                String.join(" ", "*****", "*", "*", "*****", "    *", "    *", "*****"),
+        };
+
+        for (int i = 0; i < 7; i++) {
+            String line = String.join("   ",
+                    getLine(o1[0], i),
+                    getLine(o2[0], i),
+                    getLine(p[0], i),
+                    getLine(s[0], i)
+            );
+            System.out.println(line);
+        }
+    }
+
+    static String getLine(String pattern, int index) {
+        String[] lines = pattern.split(" ");
+        return lines[index];
     }
 }
