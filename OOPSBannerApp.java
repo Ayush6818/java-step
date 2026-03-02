49b69487e5e22fbe756dabb3ae87fa149ad34260
Ayush6818 <ayush.wwekumar2633@gmail.com>
2026-03-02 23:09:39 +0530

    UC3 completed - Banner using String.join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,56 +1,50 @@
-public class OOPSBannerUC3 {
+public class OOPSBannerApp {
 
-    static void line1() {
-        System.out.println(" *****   *****   *****   ***** ");
-    }
-
-    static void line2() {
-        System.out.println(" *   *   *   *   *       *     ");
-    }
-
-    static void line3() {
-        System.out.println(" *   *   *****   *****   ***** ");
-    }
-
-    static void line4() {
-        System.out.println(" *   *   *           *       * ");
-    }
-
-    static void line5() {
-        System.out.println(" *****   *       *****   ***** ");
+    static String getLine(String pattern, int index) {
+        String[] lines = pattern.split(",");
+        return lines[index];
     }
 
     public static void main(String[] args) {
 
-        String[] o1 = {
-                String.join(" ", "*****", "*   *", "*   *", "*   *", "*   *", "*   *", "*****"),
-        };
+        String o = String.join(",",
+                " ***** ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " ***** "
+        );
 
-        String[] o2 = {
-                String.join(" ", "*****", "*   *", "*   *", "*   *", "*   *", "*   *", "*****"),
-        };
+        String p = String.join(",",
+                " ***** ",
+                " *   * ",
+                " *   * ",
+                " ***** ",
+                " *     ",
+                " *     ",
+                " *     "
+        );
 
-        String[] p = {
-                String.join(" ", "*****", "*   *", "*   *", "*****", "*", "*", "*"),
-        };
-
-        String[] s = {
-                String.join(" ", "*****", "*", "*", "*****", "    *", "    *", "*****"),
-        };
+        String s = String.join(",",
+                " ***** ",
+                " *     ",
+                " *     ",
+                " ***** ",
+                "     * ",
+                "     * ",
+                " ***** "
+        );
 
         for (int i = 0; i < 7; i++) {
             String line = String.join("   ",
-                    getLine(o1[0], i),
-                    getLine(o2[0], i),
-                    getLine(p[0], i),
-                    getLine(s[0], i)
+                    getLine(o, i),
+                    getLine(o, i),
+                    getLine(p, i),
+                    getLine(s, i)
             );
             System.out.println(line);
         }
-    }
-
-    static String getLine(String pattern, int index) {
-        String[] lines = pattern.split(" ");
-        return lines[index];
     }
 }
